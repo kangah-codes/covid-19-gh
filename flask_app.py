ca244873d8b4e8131db8180fec741e770c782229
Joshua Akangah <akangah89@hmail.com>
2020-03-20 10:29:17 +0000

    error handling

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, abort
 from scraper import get_data
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
@@ -7,14 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	url = "https://ghanahealthservice.org/covid19/"
-	req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
-	webpage = urlopen(req).read()
-	page_soup = soup(webpage, 'html.parser')
-	item = page_soup.find('span', {'align':'center'})
-	data = get_data()
-	data['new'] = item.text
+	try:
+		url = "https://ghanahealthservice.org/covid19/"
+		req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
+		webpage = urlopen(req).read()
+		page_soup = soup(webpage, 'html.parser')
+		item = page_soup.find('span', {'align':'center'})
+		data = get_data()
+		data['new'] = item.text
+	except:
+		abort(500)
 	return render_template('index.html', **data)
+
+@app.errorhandler(500)
+def handler(error):
+	return render_template('error.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
