8857bf3103ceaa91fb4d2bb26e3a181ae5adb2ea
Joshua Akangah <akangah89@hmail.com>
2020-03-21 10:21:25 +0000

    new

--- flask_app.py
+++ flask_app.py
@@ -8,17 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	# try:
-	# 	# url = "https://ghanahealthservice.org/covid19/"
-	# 	# req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
-	# 	# webpage = urlopen(req).read()
-	# 	# page_soup = soup(webpage, 'html.parser')
-	# 	# item = page_soup.find('span', {'align':'center'})
-	data = get_data()
-	# 	# data['new'] = item.text
-	# 	pass
-	# except:
-	# 	abort(500)
+	try:
+		url = "https://ghanahealthservice.org/covid19/"
+		req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
+		webpage = urlopen(req).read()
+		page_soup = soup(webpage, 'html.parser')
+		item = page_soup.find('span', {'align':'center'})
+		data = get_data()
+		data['new'] = item.text
+	except:
+		abort(500)
 	now = datetime.datetime.now()
 	data['now'] = now
 	return render_template('index.html', **data)
