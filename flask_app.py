cc9c1d6b33e547439224e2e6dcf72f2490900dbb
Joshua Akangah <akangah89@hmail.com>
2020-03-24 12:25:52 +0000

    new

--- flask_app.py
+++ flask_app.py
@@ -8,32 +8,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	data = {}
-	try:
-		url = "https://ghanahealthservice.org/covid19/"
-		req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
-		webpage = urlopen(req).read()
-		page_soup = soup(webpage, 'html.parser')
-		item = page_soup.findAll('p', {'class':'information-line-text'})
-		dat = get_data()
-		data['new_cases'] = dat['new_cases']
-		data['new_deaths'] = dat['new_deaths']
-		data['bool'] = True
-		data['new'] = []
-		for i in item:
-			print(i.text)
-			data['new'].append(i.text)
-		print(data['new'])
-		now = datetime.datetime.now()
-		data['now'] = now
-		return render_template('index.html', **data)
-	except Exception as e:
-		data['bool'] = False
-		print("errpr")
-		data = get_data()
-		now = datetime.datetime.now()
-		data['now'] = now
-		return render_template('index.html', **data)
+	#data = {}
+	# try:
+	# 	url = "https://ghanahealthservice.org/covid19/"
+	# 	req = Request(url, headers={'User-Agent':'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'})
+	# 	webpage = urlopen(req).read()
+	# 	page_soup = soup(webpage, 'html.parser')
+	# 	item = page_soup.findAll('p', {'class':'information-line-text'})
+	# 	dat = get_data()
+	# 	data['new_cases'] = dat['new_cases']
+	# 	data['new_deaths'] = dat['new_deaths']
+	# 	data['bool'] = True
+	# 	data['new'] = []
+	# 	for i in item:
+	# 		print(i.text)
+	# 		data['new'].append(i.text)
+	# 	print(data['new'])
+	# 	now = datetime.datetime.now()
+	# 	data['now'] = now
+	# 	return render_template('index.html', **data)
+	# except Exception as e:
+	data['bool'] = False
+	print("errpr")
+	data = get_data()
+	now = datetime.datetime.now()
+	data['now'] = now
+	return render_template('index.html', **data)
 
 @app.errorhandler(500)
 def handler(error):
